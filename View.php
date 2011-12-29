bfff49ccfd9adb1a3185b298becf581ac803a9a0
Bibek Shrestha <bibek@yipl.com.np>
2011-12-29 12:43:03 +0100

    first commit

---
+++ View.php
@@ -0,0 +1,25 @@
+<?php
+class View
+{
+    public function assign($vars)
+    {
+        foreach ($vars as $key => $value) {
+            if ( '_' != substr($key, 0, 1)) $this->{$key} = $value;
+        }
+    }
+
+    public function render($_template, $_return = false)
+    {
+        if ( $_return ) {
+            ob_start();
+            require_once($_template);
+            $output = ob_get_clean();
+            return $output;
+        }
+        require_once($_template);
+    }
+}
+
+$view = new View();
+$view->assign(array('a' => 'AA', 'b' => 'BB', 'c' => 'CC'));
+print $view->render('file.tpl', true);
